7ca6620fee0b41136d91f836f1df38a0ea8ddc9e
Ruben Granet <ruben.granet@icloud.com>
2022-11-29 17:48:07 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ruben Granet on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
